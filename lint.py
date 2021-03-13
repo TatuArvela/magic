7c3014d11006f1947064b40b5fa1de28170ce7cd
Tatu Arvela <tatu.arvela@nitor.com>
2021-03-13 23:39:27 +0200

    Improve tooling, add bandit

--- lint.py
+++ lint.py
@@ -1,16 +1,17 @@
 import subprocess
 
-BLUE = "\033[0;34m"
-NO_COLOR = "\033[0m"
+from magic.utils.display import Colors, in_color
 
 
 def lint():
-    print(f"{BLUE}# Running isort{NO_COLOR}")
+    print(in_color("# Running isort", Colors.BLUE))
     subprocess.run(["isort", "."])
-    print(f"{BLUE}# Running black{NO_COLOR}")
+    print(in_color("# Running black", Colors.BLUE))
     subprocess.run(["black", "."])
-    print(f"{BLUE}# Running flake8{NO_COLOR}")
+    print(in_color("# Running flake8", Colors.BLUE))
     subprocess.run(["flake8", "."])
+    print(in_color("# Running bandit", Colors.BLUE))
+    subprocess.run(["bandit", "-r", "--ini", ".bandit"])
 
 
 if __name__ == "__main__":
