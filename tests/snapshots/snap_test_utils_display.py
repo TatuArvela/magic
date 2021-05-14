6e7d7054d76594a1f2ab52a29593036f418a99e2
Tatu Arvela <tatu.arvela@nitor.com>
2021-05-14 17:10:35 +0300

    Ignore snapshots in pre-commit-config

--- tests/snapshots/snap_test_utils_display.py
+++ tests/snapshots/snap_test_utils_display.py
@@ -4,19 +4,20 @@ from __future__ import unicode_literals
 
 from snapshottest import Snapshot
 
+
 snapshots = Snapshot()
 
-snapshots["test_clear_last_line 1"] = "\x1b[F\x1b[K"
+snapshots['test_clear_last_line 1'] = '\x1b[F\x1b[K'
 
-snapshots["test_in_color 1"] = [
-    "\x1b[30mtest\x1b[0m",
-    "\x1b[31mtest\x1b[0m",
-    "\x1b[32mtest\x1b[0m",
-    "\x1b[33mtest\x1b[0m",
-    "\x1b[34mtest\x1b[0m",
-    "\x1b[35mtest\x1b[0m",
-    "\x1b[36mtest\x1b[0m",
-    "\x1b[37mtest\x1b[0m",
+snapshots['test_in_color 1'] = [
+    '\x1b[30mtest\x1b[0m',
+    '\x1b[31mtest\x1b[0m',
+    '\x1b[32mtest\x1b[0m',
+    '\x1b[33mtest\x1b[0m',
+    '\x1b[34mtest\x1b[0m',
+    '\x1b[35mtest\x1b[0m',
+    '\x1b[36mtest\x1b[0m',
+    '\x1b[37mtest\x1b[0m'
 ]
 
-snapshots["test_print_error 1"] = "\x1b[31m🔥 Error: test\x1b[0m"
+snapshots['test_print_error 1'] = '\x1b[31m🔥 Error: test\x1b[0m'
