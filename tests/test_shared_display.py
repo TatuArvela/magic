1997bd10d97503bd95edfd0bc264cefa905a99e8
Tatu Arvela <tatu.arvela@nitor.com>
2021-09-06 19:48:24 +0300

    Use strip_output util in shared_display tests, update naming

--- tests/test_shared_display.py
+++ tests/test_shared_display.py
@@ -2,6 +2,7 @@ import contextlib
 from io import StringIO
 
 from magic.shared.display import Color, clear_last_line, in_color, print_error
+from tests.utils import strip_output
 
 
 def test_in_color(snapshot):
@@ -15,7 +16,7 @@ def test_print_error(snapshot):
     temp_stdout = StringIO()
     with contextlib.redirect_stdout(temp_stdout):
         print_error("test")
-    output = temp_stdout.getvalue().strip()
+    output = strip_output(temp_stdout)
     snapshot.assert_match(output)
 
 
@@ -23,5 +24,5 @@ def test_clear_last_line(snapshot):
     temp_stdout = StringIO()
     with contextlib.redirect_stdout(temp_stdout):
         clear_last_line()
-    output = temp_stdout.getvalue().strip()
+    output = strip_output(temp_stdout)
     snapshot.assert_match(output)
